940832bc96e6f8abc9f83eedb4e70ab2d26aa819
chenxxxxxxxxxwang <xing_wang_2012@hotmail.com>
2016-03-31 14:33:42 +0800

    check
    自定义View 实现单眼 视图播放

---
+++ VRPictureBrowser/VRPictureBrowser/XW_SelfDefineView.h
@@ -0,0 +1,18 @@
+//
+//  XW_SelfDefineView.h
+//  VRPictureBrowser
+//
+//  Created by chenxingwang on 16/3/31.
+//  Copyright © 2016年 PaPaTV.COM. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface XW_SelfDefineView : GLKView
+
+/**try to self define a single view picture *browser/
+
+
+
+
+@end
---
+++ VRPictureBrowser/VRPictureBrowser/XW_SelfDefineView.m
@@ -0,0 +1,21 @@
+//
+//  XW_SelfDefineView.m
+//  VRPictureBrowser
+//
+//  Created by chenxingwang on 16/3/31.
+//  Copyright © 2016年 PaPaTV.COM. All rights reserved.
+//
+
+#import "XW_SelfDefineView.h"
+
+@implementation XW_SelfDefineView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
